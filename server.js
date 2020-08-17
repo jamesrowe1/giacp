bd8d0e50281ce635cce2b7585b6836076557fea0
James Rowe <jamesrowe@outlook.com>
2020-08-17 18:04:16 -0400

    server

---
+++ server.js
@@ -0,0 +1,24 @@
+// Requiring necessary npm packages
+require("dotenv").config();
+const express = require("express");
+const session = require("express-session");
+
+
+// Setting up port and requiring models for syncing
+const PORT = process.env.PORT || 8080;
+
+// Creating express app and configuring middleware needed for authentication
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// Set Handlebars.
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Requiring our routes
+require("./routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
