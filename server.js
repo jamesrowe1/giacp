f3553d42e7b6a70ab1bf9cdbfc316dc1842d1492
James Rowe <jamesrowe@outlook.com>
2020-08-17 18:39:19 -0400

    fixed handlebars

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ require("dotenv").config();
 const express = require("express");
 const session = require("express-session");
 
-
 // Setting up port and requiring models for syncing
 const PORT = process.env.PORT || 8080;
 
