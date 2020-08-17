cde3f9fd976489d2ae9e26d58cd70d9c7c282a19
James Rowe <jamesrowe@outlook.com>
2020-08-17 18:14:09 -0400

    added head and foot

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,23 +1,23 @@
 // Requiring path to so we can use relative routes to our HTML files
 const path = require("path");
 
-module.exports = function (app) {
-    app.get("/", (req, res) => {
-        res.render("index", { layout: "main" })
-    })
-    app.get("/aboutus", (req, res) => {
-        res.render("aboutus", { layout: "main" })
-    })
-    app.get("/associates", (req, res) => {
-        res.render("associates", { layout: "main" })
-    })
-    app.get("/colonoscopy", (req, res) => {
-        res.render("colonoscopy", { layout: "main" })
-    })
-    app.get("/upperendoscopy", (req, res) => {
-        res.render("upperendoscopy", { layout: "main" })
-    })
-    app.get("/contactus", (req, res) => {
-        res.render("aboutus", { layout: "main" })
-    })
-}
+module.exports = function(app) {
+  app.get("/", (req, res) => {
+    res.render("index", { layout: "main" });
+  });
+  app.get("/aboutus", (req, res) => {
+    res.render("aboutus", { layout: "main" });
+  });
+  app.get("/associates", (req, res) => {
+    res.render("associates", { layout: "main" });
+  });
+  app.get("/colonoscopy", (req, res) => {
+    res.render("colonoscopy", { layout: "main" });
+  });
+  app.get("/upperendoscopy", (req, res) => {
+    res.render("upperendoscopy", { layout: "main" });
+  });
+  app.get("/contactus", (req, res) => {
+    res.render("aboutus", { layout: "main" });
+  });
+};
