48ba4a32e8aaa0c34cbfdc20faf91efa6bf32e54
James Rowe <jamesrowe@outlook.com>
2020-08-17 21:51:39 -0400

    fixing webpages

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -18,6 +18,35 @@ module.exports = function(app) {
     res.render("upperendoscopy", { layout: "main" });
   });
   app.get("/contactus", (req, res) => {
-    res.render("aboutus", { layout: "main" });
+    res.redirect(
+      "https://www.medentmobile.com/portal/index.php?practice_id=9GCvbWd2"
+    );
+  });
+  app.get("/24hrph", (req, res) => {
+    res.render("24hrph", { layout: "main" });
+  });
+  app.get("/directions", (req, res) => {
+    res.render("directions", { layout: "main" });
+  });
+  app.get("/ercp", (req, res) => {
+    res.render("ercp", { layout: "main" });
+  });
+  app.get("/esophagealmanometry", (req, res) => {
+    res.render("esophagealmanometry", { layout: "main" });
+  });
+  app.get("/flexiblesigmoidoscopy", (req, res) => {
+    res.render("flexiblesigmoidoscopy", { layout: "main" });
+  });
+  app.get("/insurance", (req, res) => {
+    res.render("insurance", { layout: "main" });
+  });
+  app.get("/jcaho", (req, res) => {
+    res.render("jcaho", { layout: "main" });
+  });
+  app.get("/liverbiopsy", (req, res) => {
+    res.render("liverbiopsy", { layout: "main" });
+  });
+  app.get("/screeningcolonoscopy", (req, res) => {
+    res.render("screeningcolonoscopy", { layout: "main" });
   });
 };
