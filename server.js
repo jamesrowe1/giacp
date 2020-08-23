b3527200d3204c03feedf4b10c7e5b74635e8df3
James Rowe <jamesrowe@outlook.com>
2020-08-23 13:37:38 -0400

    installable!

--- server.js
+++ server.js
@@ -31,3 +31,38 @@ app.listen(PORT, () => {
     PORT
   );
 });
+
+const workboxBuild = require("workbox-build");
+
+// NOTE: This should be run *AFTER* all your assets are built
+const buildSW = () => {
+  // This will return a Promise
+  return workboxBuild.generateSW({
+    globDirectory: "/",
+    globPatterns: ["**/*.{html,json,js,css}"],
+    swDest: "sw.js",
+
+    // Define runtime caching rules.
+    runtimeCaching: [
+      {
+        // Match any request that ends with .png, .jpg, .jpeg or .svg.
+        urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
+
+        // Apply a cache-first strategy.
+        handler: "CacheFirst",
+
+        options: {
+          // Use a custom cache name.
+          cacheName: "images",
+
+          // Only cache 10 images.
+          expiration: {
+            maxEntries: 10
+          }
+        }
+      }
+    ]
+  });
+};
+
+buildSW();
